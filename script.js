701a5d46cede3e098fa40a98ab451faebc3eb945
Manikanta Itikela <113256332+Manikanta79i@users.noreply.github.com>
2023-04-10 13:29:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-//your JS code here. If required.
+function firstNonRepeatingChar(str) {
+  // Create a map to store the count of each character
+  const charCount = new Map();
+  for (const char of str) {
+    charCount.set(char, (charCount.get(char) || 0) + 1);
+  }
+
+  // Find the first character with a count of 1
+  for (const char of str) {
+    if (charCount.get(char) === 1) {
+      return char;
+    }
+  }
+
+  // No non-repeating character found
+  return null;
+}
+
+// Prompt user for input and display result
+const input = prompt("Enter a string:");
+const result = firstNonRepeatingChar(input);
+alert(result ? `The first non-repeating character is "${result}"` : "No non-repeating character found.");
